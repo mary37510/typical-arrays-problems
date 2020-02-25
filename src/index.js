3834ea72f1b2145761e6f74923090f8f2d75ab37
Mary Zhavnerovich <rostova0106@gmail.com>
2020-02-25 12:09:43 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
+  if (Array.isArray(array) === true && array.length !== 0) {
+  return Math.min.apply(null, array);
+  } else {
   return 0;
 }
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (Array.isArray(array) === true && array.length !== 0) {
+  return Math.max.apply(null, array);
+    } else {
+    return 0;
+  }
+};
 
 exports.avg = function avg (array) {
+  if (Array.isArray(array) === true && array.length !== 0) {
+    return array.reduce((a, b) => (a+b))/array.length;
+  } else {
   return 0;
-}
+  }
+};
